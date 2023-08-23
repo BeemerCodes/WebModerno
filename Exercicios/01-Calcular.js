2aa70b23c155daada26c2a6a35c415cbb3289e34
BeemerCodes <detonainyoutub@gmail.com>
2023-08-23 20:33:44 -0300

    Wip

---
+++ Exercicios/01-Calcular.js
@@ -0,0 +1,14 @@
+let value1 = 0, value2 = 0;
+
+function Calculator(value1, value2){
+
+    console.log("------------------------------------------------------")
+    console.log(`A Soma Resultou em: ${value1 + value2}`)
+    console.log(`A Subtração Resultou em: ${value1 - value2}`)
+    console.log("A Multiplicação Resultou em: " + value1 * value2)
+    console.log("A Divisão Resultou em: " + value1 / value2)
+    console.log("-----_-----_-----_-----_-----_-----_-----_-----_-----_")
+
+}
+
+Calculator(10, 5)
